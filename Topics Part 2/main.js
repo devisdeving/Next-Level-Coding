0dc6fa6e60d5160200f426cb04e6fed276c3446a
devisdeving <makkd805@newschool.edu>
2023-10-10 10:33:56 -0400

    base setup done

--- Topics Part 2/main.js
+++ Topics Part 2/main.js
@@ -1,4 +1,20 @@
 const gallery = document.getElementById("gallery");
+const shapes = document.querySelectorAll('.shape');
+
+// Shuffle the grid items using the Fisher-Yates algorithm
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+shuffle(Array.from(shapes));
+
+// Append the shuffled grid items back to the grid container
+shapes.forEach((shape) => {
+    gallery.appendChild(shape);
+});
 
 window.onmousemove = e => {
     const mouseX = e.clientX,
@@ -7,17 +23,32 @@ window.onmousemove = e => {
     const xDecimal = mouseX / window.innerWidth,
           yDecimal = mouseY / window.innerHeight;
 
-    // const maxX = gallery.offsetWidth - window.innerWidth,
-    //       maxY = gallery.offsetHeight - window.innerHeight;
+    const maxX = gallery.offsetWidth - window.innerWidth,
+          maxY = gallery.offsetHeight - window.innerHeight;
 
-    const panX = gallery.offsetWidth * xDecimal * -1,
-          panY = gallery.offsetHeight * yDecimal * -1;
+    const panX = gallery.offsetWidth * xDecimal * -1;
+    const panY = gallery.offsetHeight * yDecimal * -1;
 
     gallery.animate({
         transform: `translate(${panX}px, ${panY}px)`
     }, {
-        duration: 4000,
+        duration: 8000,
         fill: "forwards",
         easing: "ease"
     })
 }
+
+
+
+// shapes.forEach((shape) => {
+//     const randomX = Math.floor(Math.random() * (gallery.clientWidth - shape.clientWidth));
+//     const randomY = Math.floor(Math.random() * (gallery.clientHeight - shape.clientHeight));
+
+//     shape.style.position = 'absolute';
+//     shape.style.left = randomX + 'px';
+//     shape.style.top = randomY + 'px';
+
+//     gallery.appendChild(shape);
+// });
+
+
