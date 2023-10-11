a7b02bb48d572b810b4146d884f15564befa99b3
devisdeving <makkd805@newschool.edu>
2023-10-10 21:31:32 -0400

    video works

--- Topics Part 2/main.js
+++ Topics Part 2/main.js
@@ -1,72 +1,78 @@
 const gallery = document.getElementById("gallery");
 const shapes = document.querySelectorAll('.shape');
 
-// Shuffle the grid items using the Fisher-Yates algorithm
-function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-}
-
-shuffle(Array.from(shapes));
+  window.onload = () => {
+    shapes.forEach((shape) => {
+        const randomScale = Math.random() * (0.5 - 0.8);
+        shape.style.transform = `scale(${randomScale})`;
+    });
+};
 
 shapes.forEach((shape) => {
-    gallery.appendChild(shape);
+    const maxTop = window.innerHeight * 0.8; // 80% of the viewport's height
+    const maxLeft = window.innerWidth * 0.8; // 80% of the viewport's width
+
+    const randomTop = Math.random() * maxTop;
+    const randomLeft = Math.random() * maxLeft;
+
+    shape.style.top = randomTop + 'px';
+    shape.style.left = randomLeft + 'px';
 });
 
-window.onmousemove = e => {
-    const mouseX = e.clientX,
-          mouseY = e.clientY;
 
-    const xDecimal = mouseX / window.innerWidth,
-          yDecimal = mouseY / window.innerHeight;
-
-    const maxX = gallery.offsetWidth - window.innerWidth,
-          maxY = gallery.offsetHeight - window.innerHeight;
-
-    const panX = gallery.offsetWidth * xDecimal * -1;
-    const panY = gallery.offsetHeight * yDecimal * -1;
-
-    gallery.animate({
-        transform: `translate(${panX}px, ${panY}px)`
-    }, {
-        duration: 8000,
-        fill: "forwards",
-        easing: "ease"
-    })
-}
-
-  const video = document.getElementById('video');
+//   const video = document.getElementById('video');
   
-  // Define a function to handle the mousemove event
-  function handleMouseMove(event) {
-    // Calculate the horizontal position of the mouse within the video element
-    const mouseX = event.clientX - video.getBoundingClientRect().left;
+//   // Define a function to handle the mousemove event
+//   function handleMouseMove(event) {
+//     // Calculate the horizontal position of the mouse within the video element
+//     const mouseX = event.clientX - video.getBoundingClientRect().left;
     
-    // Calculate the percentage of the video's duration based on the mouse position
-    const percentage = (mouseX / video.clientWidth) * 100;
+//     // Calculate the percentage of the video's duration based on the mouse position
+//     const percentage = (mouseX / video.clientWidth) * 100;
     
-    // Set the video's playback position as a percentage of its duration
-    video.currentTime = (percentage / 100) * video.duration;
-  }
-  
-  // Add a mousemove event listener to the video element
-  video.addEventListener('mousemove', handleMouseMove);
+//     // Set the video's playback position as a percentage of its duration
+//     video.currentTime = (percentage / 100) * video.duration;
+//   }
+
+//     // Add a mousemove event listener to the video element
+//     video.addEventListener('mousemove', handleMouseMove);
 
 
 
 
+// // Shuffle the grid items using the Fisher-Yates algorithm
+// function shuffle(array) {
+//     for (let i = array.length - 1; i > 0; i--) {
+//         const j = Math.floor(Math.random() * (i + 1));
+//         [array[i], array[j]] = [array[j], array[i]];
+//     }
+// }
+
+// shuffle(Array.from(shapes));
 
 // shapes.forEach((shape) => {
-//     const randomX = Math.floor(Math.random() * (gallery.clientWidth - shape.clientWidth));
-//     const randomY = Math.floor(Math.random() * (gallery.clientHeight - shape.clientHeight));
-
-//     shape.style.position = 'absolute';
-//     shape.style.left = randomX + 'px';
-//     shape.style.top = randomY + 'px';
-
 //     gallery.appendChild(shape);
 // });
 
+// window.onmousemove = e => {
+//     const mouseX = e.clientX,
+//           mouseY = e.clientY;
+
+//     const xDecimal = mouseX / window.innerWidth,
+//           yDecimal = mouseY / window.innerHeight;
+
+//     const maxX = gallery.offsetWidth - window.innerWidth,
+//           maxY = gallery.offsetHeight - window.innerHeight;
+
+//     const panX = gallery.offsetWidth * xDecimal * -1;
+//     const panY = gallery.offsetHeight * yDecimal * -1;
+
+//     gallery.animate({
+//         transform: `translate(${panX}px, ${panY}px)`
+//     }, {
+//         duration: 8000,
+//         fill: "forwards",
+//         easing: "ease"
+//     })
+// }
 
