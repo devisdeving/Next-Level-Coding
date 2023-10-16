ae21751fe56a474690a615f6f64662b32f30c01b
devisdeving <makkd805@newschool.edu>
2023-10-16 18:22:52 -0400

    logo 1 coded

--- Project-3/main.js
+++ Project-3/main.js
@@ -3,15 +3,38 @@ function setup() {
 }
 
 function draw() {
-background(240);
+background(255);
 
 const red = color(251, 2, 7);
 const black = color(0, 0, 0);
 const white = color(255, 255, 255);
 
-vertex(61, 130);
-bezier(61, 264, 130, );
+fill(red);
+noStroke();
 
+beginShape();
+vertex(71, 58);
+bezierVertex(125, 59, 300, 70, 300, 138);
+bezierVertex(300, 138, 300, 205, 125, 216);
+vertex(71, 218);
+vertex(71, 137);
+bezierVertex(98, 137, 149, 132, 149, 110);
+bezierVertex(149, 110, 149, 83, 71, 83.1);
+vertex(71, 82.6);
+vertex(71, 58);
+endShape();
+
+beginShape();
+vertex(311, 110);
+bezierVertex(320, 119, 415, 225, 266, 300);
+bezierVertex(266, 300, 209, 329, 110, 337);
+vertex(71, 339);
+vertex(71, 253);
+bezierVertex(115, 253, 178, 250, 251, 222);
+vertex(251, 222);
+bezierVertex(318, 196, 343, 156, 311, 110);
+
+endShape();
 
 //FIRST ATTEMPT TO RECREATE LOGO WITH CONCENTRIC ELLIPSES
     //beginShape; 
