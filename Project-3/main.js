66d8d30a2baf4ef04e2f96577a8d18c5840f3d63
devisdeving <makkd805@newschool.edu>
2023-10-16 17:40:52 -0400

    ok here we go

---
+++ Project-3/main.js
@@ -0,0 +1,29 @@
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+background(240);
+
+const red = color(251, 2, 7);
+const black = color(0, 0, 0);
+const white = color(255, 255, 255);
+
+vertex(61, 130);
+bezier(61, 264, 130, );
+
+
+//FIRST ATTEMPT TO RECREATE LOGO WITH CONCENTRIC ELLIPSES
+    //beginShape; 
+    //ellipse(200, 200, 360, 180);
+    // ellipse(200, 195, 330, 160);
+    // ellipse(200, 190, 300, 140);
+    // ellipse(200, 185, 270, 120);
+    // ellipse(200, 180, 240, 100);
+    // ellipse(200, 175, 210, 80);
+
+    // fill(251, 2, 7);
+    // noStroke();
+    // ellipse(200, 170, 180, 60);
+  }
+       
