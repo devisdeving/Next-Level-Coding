e45d4cb58f39d1a3a7733c136b6c34c4a5bd8411
devisdeving <makkd805@newschool.edu>
2023-10-22 18:57:24 -0400

    mapped values!

--- Project-3/main.js
+++ Project-3/main.js
@@ -1,22 +1,95 @@
-const url = 'https://api.spotify.com/v1/tracks/4heve4ydl1u6V3AD4moZq9';
-const options = {
-    method: 'GET',
-    headers: {
-        'Authorization': 'Bearer 1POdFZRZbvb...qqillRxMr2z'
-    }
+const ripple1 = document.getElementById('ripple1');
+const ripple2 = document.getElementById('ripple2');
+const ripple3 = document.getElementById('ripple3');
+const ripple4 = document.getElementById('ripple4');
+const disc = document.getElementById('disc');
+
+const set =
+    {
+        artistName: "LP Giobbi",
+        mL: 3934806,
+        song1: "Antibodies - LP Giobbi Remix",
+        song1Streams: 26774794,
+        song2: "Forever And A Day",
+        song2Streams: 18661938,
+        song3: "All In A Dream",
+        song3Streams: 14584852,
+        song4: "Can't Let You Go",
+        song4Streams: 5387629
+    };
+    // {
+    //     artistName: "Aluna",
+    //     mL: 4229523,
+    //     song1: "Beggin'",
+    //     song1Streams: 30797317,
+    //     song2: "Forget About Me",
+    //     song2Streams: 30167368,
+    //     song3: "Body Pump - Lady Bee Remix",
+    //     song3Streams: 18736101,
+    //     song4: "More Baby",
+    //     song4Streams: 4232323
+    // },
+    // {
+    //     artistName: "Zeds Dead",
+    //     mL: 2360883,
+    //     song1: "Eyes On Fire - Zeds Dead Remix'",
+    //     song1Streams: 53597365,
+    //     song2: "Alive",
+    //     song2Streams: 30643625,
+    //     song3: "Gassed Up",
+    //     song3Streams: 15502516,
+    //     song4: "One Three Nine",
+    //     song4Streams: 8863126
+    // }
+
+function e() {
+
+    const monthly = set.mL / 70000;
+    const songOneStreams = set.song1Streams / 200000;
+    const songTwoStreams = set.song2Streams / 200000;
+    const songThreeStreams = set.song3Streams / 200000;
+    const songFourStreams = set.song4Streams / 200000;
+
+    const songOneStreamsStyle = map(songOneStreams, 0, 100, 80, 140);
+    const songTwoStreamsStyle = map(songTwoStreams, 0, 100, 80, 140);
+    const songThreeStreamsStyle = map(songThreeStreams, 0, 100, 80, 140);
+    const songFourStreamsStyle = map(songFourStreams, 0, 100, 80, 140);
+    const monthlyStyle = map(monthly, 0, 100, 80, 140);
+
+    ripple1.style.transform = `scaleX(${songOneStreamsStyle}%)`;
+    ripple2.style.transform = `scaleX(${songTwoStreamsStyle}%)`;
+    ripple3.style.transform = `scaleX(${songThreeStreamsStyle}%)`;
+    ripple4.style.transform = `scaleX(${songFourStreamsStyle}%)`;
+    disc.style.transform = `scale(${monthlyStyle}%)`;
 };
 
-fetch(url, options)
-    .then(function (response) {
-        return response.text();
-    })
-    .then(function (data) {
-        console.log(data);
-        // Handle the data here
-    })
-    .catch(function (error) {
-        console.error("Something went wrong!", error);
-    });
+e();
+
+    function map(value, low1, high1, low2, high2) {
+        return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
+    }
+
+
+// SPOTIFY API ATTEMPT  
+// const url = 'https://api.spotify.com/v1/tracks/4heve4ydl1u6V3AD4moZq9';
+// const options = {
+//     method: 'GET',
+//     headers: {
+//         'Authorization': 'Bearer 1POdFZRZbvb...qqillRxMr2z'
+//     }
+// };
+
+// fetch(url, options)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//         // Handle the data here
+//     })
+//     .catch(function (error) {
+//         console.error("Something went wrong!", error);
+//     });
 
 
 // let myUrl = 'https://api.weatherapi.com/v1/current.json?key=43a0610734b24e6f82505625231710&q=London&aqi=no';
