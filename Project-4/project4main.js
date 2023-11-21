2ada2f2b2540804616fdb2de983d1e94efeb0fdb
devisdeving <makkd805@newschool.edu>
2023-11-20 21:49:22 -0500

    input removed

--- Project-4/project4main.js
+++ Project-4/project4main.js
@@ -1,11 +1,5 @@
 
 const dataParent = document.getElementById("dataGoesHere");
-const feelingSlider = document.getElementById("feelingSlider");
-const topSlider = document.getElementById("topSlider");
-const colorInputs = document.getElementsByClassName("colorInput");
-const colorInput1 = document.getElementById("colorInput1");
-const colorInput2 = document.getElementById("colorInput2");
-const colorInput3 = document.getElementById("colorInput3");
 const body = document.querySelector("body");
 const mGDotSVG = `<path d="M102 27C102 41.9117 89.9117 54 75 54C60.0883 54 48 41.9117 48 27C48 12.0883 60.0883 0 75 0C89.9117 0 102 12.0883 102 27Z" fill="white"/>
 <path d="M249 27C249 41.9117 236.912 54 222 54C207.088 54 195 41.9117 195 27C195 12.0883 207.088 0 222 0C236.912 0 249 12.0883 249 27Z" fill="white"/>
@@ -34,22 +28,13 @@ fetch(opensheet_uri)
     .then(function (data) {
         console.log(data);
 
-        const threeParts = `
-        <img id="mask" src="perfumeMask.png">
-        <div id="topPart" class="part topPart"></div>
-        <div id="midPart" class="part midPart"></div>
-        <div id="botPart" class="part botPart"></div>
-        `;
-
-        dataParent.insertAdjacentHTML('beforeend', threeParts);
-
         const topPart = document.getElementById("topPart");
         const midPart = document.getElementById("midPart");
         const botPart = document.getElementById("botPart");
 
-        colorInput1.addEventListener("input", changeTopColor);
-        colorInput2.addEventListener("input", changeMidColor);
-        colorInput3.addEventListener("input", changeBotColor);
+        // colorInput1.addEventListener("input", changeTopColor);
+        // colorInput2.addEventListener("input", changeMidColor);
+        // colorInput3.addEventListener("input", changeBotColor);
 
         for (let datapoint of data) {
         const satMapped = datapoint.satisfaction;
@@ -95,19 +80,7 @@ fetch(opensheet_uri)
           dots.style.top = Math.random() * 100 + "%";
           dots.style.transform = `rotate(${Math.random() * 360}deg)`;
           dots.style.backgroundColor = randomFromArray(colors);
-          feelingSlider.addEventListener("input", changeMargin);
-          topSlider.addEventListener("input", changePosition);
           botPart.appendChild(dots);
-
-          function changeMargin(){
-              dots.style.margin = feelingSlider.value + "em";
-        }
-        function changePosition() {
-          let topDots = document.querySelectorAll('#topPart .confetti');
-          topDots.forEach(function (dot) {
-            dot.style.top = topSlider.value + "px";
-          });
-        }
 
     }
       }
@@ -118,26 +91,26 @@ imageDiv.id = "imageDiv";
 // Append imageDiv to the body
 document.body.appendChild(imageDiv);
 
-      function changeTopColor() {
-        let topDots = document.querySelectorAll('#topPart .confetti');        
-        topDots.forEach(function (dot) {
-          dot.style.backgroundColor = colorInput1.value;
-        });
-      }
+      // function changeTopColor() {
+      //   let topDots = document.querySelectorAll('#topPart .confetti');        
+      //   topDots.forEach(function (dot) {
+      //     dot.style.backgroundColor = colorInput1.value;
+      //   });
+      // }
 
-      function changeMidColor() {
-        let midDots = document.querySelectorAll('#midPart .confetti');
-        midDots.forEach(function (dot) {
-          dot.style.backgroundColor = colorInput2.value;
-        });
-      }
+      // function changeMidColor() {
+      //   let midDots = document.querySelectorAll('#midPart .confetti');
+      //   midDots.forEach(function (dot) {
+      //     dot.style.backgroundColor = colorInput2.value;
+      //   });
+      // }
 
-      function changeBotColor() {
-        let botDots = document.querySelectorAll('#botPart .confetti');
-        botDots.forEach(function (dot) {
-          dot.style.backgroundColor = colorInput3.value;
-        });
-      }
+      // function changeBotColor() {
+      //   let botDots = document.querySelectorAll('#botPart .confetti');
+      //   botDots.forEach(function (dot) {
+      //     dot.style.backgroundColor = colorInput3.value;
+      //   });
+      // }
 
       function appendSVG(parent) {
                 let svgContainer = document.createElement("SVG");
