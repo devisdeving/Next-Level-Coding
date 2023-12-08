f2e73167043fee948705c81bc34627cd45870da4
devisdeving <makkd805@newschool.edu>
2023-12-08 00:01:22 -0500

    turbulence and draw working

--- Project-5/main.js
+++ Project-5/main.js
@@ -1,18 +1,70 @@
+let drawingDots = [];
+
 function setup() {
-    createCanvas(600, 600);
+  createCanvas(windowWidth, windowHeight);
+  background(0,0);
+  // Load points from local storage
+  drawingDots = JSON.parse(localStorage.getItem('array-of-dots') || '[]');
 }
 
 function draw() {
-    fill(0,0,0);
-    ellipse(mouseX, mouseY, 100, 100);
+  // Continuously update and redraw the canvas
+  updateCanvas();
 }
 
-if (axis === X_AXIS) {
-    // Left to right gradient
-    for (let i = x; i <= x + w; i++) {
-      let inter = map(i, x, x + w, 0, 1);
-      let c = lerpColor(c1, c2, inter);
-      stroke(c);
-      line(i, y, i, y + h);
-    }
+function mouseDragged() {
+  const point = {
+    x: mouseX,
+    y: mouseY,
+    time: millis() // Current timestamp
+  };
+  drawingDots.push(point);
+  localStorage.setItem('array-of-dots', JSON.stringify(drawingDots));
+}
+
+function updateCanvas() {
+  clear();
+  background(0,0);
+
+  for (let point of drawingDots) {
+    drawDot(point);
   }
+}
+
+function drawDot(point) {
+  const elapsedTime = millis() - point.time;
+  const fadeDuration = 30000;
+  let alpha = 255;
+
+  if (elapsedTime < fadeDuration) {
+    alpha = 255 * (1 - (elapsedTime / fadeDuration));
+  }
+
+  noStroke();
+
+  // Define the gradient colors
+  let color1 = color("#F3EBD4");
+  let color2 = color("#B9B9B9");
+
+  // Interpolate between the two colors based on the elapsed time
+  let blendedColor = lerpColor(color1, color2, elapsedTime / fadeDuration);
+
+  // Set the fill color to the interpolated color with adjusted alpha
+  fill(red(blendedColor), green(blendedColor), blue(blendedColor), alpha);
+
+  ellipse(point.x, point.y, 30);
+  // ellipse(point.x + random(-5, 5), point.y+random(-5, 5), 30);
+}
+
+function clearDrawing() {
+  localStorage.removeItem('array-of-dots');
+  drawingDots = [];
+  clear();
+  background(0,0);
+}
+
+function keyPressed(){
+  if( key === ' '){
+    clearDrawing() 
+  }
+}
